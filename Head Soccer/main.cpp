47caa5e227472fade70c80fa4d74f25db19bf39a
v01dc0d3 <at200000000@gmail.com>
2020-03-21 13:06:35 +0200

    Fixed Button Bounds

--- Head Soccer/main.cpp
+++ Head Soccer/main.cpp
@@ -78,15 +78,15 @@ struct Button
 
     void lock()
     {
-        frame.setTextureRect(sf::IntRect(size.x * 2,0,size.x,size.y));
+        frame.setTextureRect(sf::IntRect(size.x * 2 + 24,0,size.x - 30,size.y));
     }
     void clicked()
     {
-        frame.setTextureRect(sf::IntRect(0,0,size.x,size.y));
+        frame.setTextureRect(sf::IntRect(0,0,size.x - 30,size.y));
     }
     void notClicked()
     {
-         frame.setTextureRect(sf::IntRect(size.x + 12,0,size.x - 20,size.y - 10));
+         frame.setTextureRect(sf::IntRect(size.x + 12,0,size.x - 30,size.y - 10));
     }
 
     void create(sf::Vector2f pos,std::string x,float shift = 0)
